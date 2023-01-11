bd7f320206e0e93f676f926170f8cade585aceb2
Eric Leduc <eriled@gmail.com>
2023-01-11 11:59:24 -0500

    add file

--- src/MainProject2.java
+++ src/MainProject2.java
@@ -1,5 +1,6 @@
-public class Main {
+public class MainProject2 {
     public static void main(String[] args) {
-        System.out.println("Hello Fidelis!");
+        System.out.println("Hello from project 2!");
+        MainProject1.main(null);
     }
 }
